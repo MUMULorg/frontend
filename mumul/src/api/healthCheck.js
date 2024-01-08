2b16d8688d7e40f1b86bd22d07b856b7505a0c23
silver0-stack <dev.choiey@gmail.com>
2024-01-08 10:51:16 +0900

    aa

---
+++ mumul/src/api/healthCheck.js
@@ -0,0 +1,13 @@
+// Axios를 사용하는 경우
+import axios from 'axios';
+
+const healthCheck = async () => {
+  try {
+    const response = await axios.get('/health'); // 헬스 체크를 수행할 엔드포인트
+    console.log('Health Check Response:', response.data);
+    // 여기에서 응답을 분석하고 필요한 동작을 수행할 수 있습니다.
+  } catch (error) {
+    console.error('Health Check Failed:', error.message);
+    // 헬스 체크에 실패한 경우 에러를 처리합니다.
+  }
+};
