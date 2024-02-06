49e588ffdf0a463f7e10412771bb4260bbdbc986
silver0-stack <dev.choiey@gmail.com>
2024-02-06 14:42:29 +0900

    no-cache

--- mumul/src/pages/Login.js
+++ mumul/src/pages/Login.js
@@ -42,6 +42,7 @@ const Login = ({ isLogin, setIsLogin, hasRequestedCallback, setHasRequestedCallb
             'Content-Type': 'application/json',
           },
           //withCredentials: true, //-> cors 문제 해결 위해 일단 주석처리
+          cache: 'no-cache', // 캐시를 비우는 옵션 추가
           crossDomain: true,
         })
 
